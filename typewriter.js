5e42931b19c299cddc016aec07d040dfc0fcfeac
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2020-09-02 15:16:24 +0200

    makes typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,16 @@
 "use strict";
+const typeWritten = document.querySelector(".typewritten");
+const text = typeWritten.textContent;
+let i = 0;
+typeWrite();
+
+function typeWrite() {
+    setTimeout(()=> {
+        if(i !==text.length-1) {
+            let newText = text.substring(0,i+1);
+            typeWritten.textContent = newText;
+            i++;
+            typeWrite();
+        }
+    }, Math.floor(Math.random()*600)+300);
+}
